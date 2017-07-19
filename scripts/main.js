a423b0cef282bb285237ca9f48a35f2b5dea86f0
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-19 14:37:43 -0400

    adds random num generating function

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+var firstNumbers = addNumbers(69);
+var secondNumbers = addNumbers(26);
+var nums = assignNumbers();
+console.log(nums);
+
+
+function addNumbers(num) {
+    var arr = [];
+    for (var i=1;i<=num;i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+function assignNumbers() {
+    var numbers = []
+    for (var i=1;i<=5;i++) {
+        var randomDecimal = Math.random();
+        var len = firstNumbers.length;
+        var index = Math.round(randomDecimal * len);
+        var num = 0;
+        if (index === 0){
+            num = firstNumbers[index];
+        } else if (index ===1) {
+            num = firstNumbers[index-1]
+        } else {
+            num = firstNumbers[index-2];
+        }
+        firstNumbers.splice(index-2,1);
+        numbers.push(num);
+    }
+    var newRandomDecimal = Math.random();
+    var secondLen = secondNumbers.length;
+    var index2 = Math.round(newRandomDecimal * secondLen);
+    var num2 = 0;
+        if (index2 === 0){
+            num2 = firstNumbers[index2];
+        } else if (index2 ===1) {
+            num2 = firstNumbers[index2-1]
+        } else {
+            num2 = firstNumbers[index2-2];
+        }
+        numbers.push(num2);
+    return numbers;
+}
